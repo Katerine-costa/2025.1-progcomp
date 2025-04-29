bd51100e01a829c5af8137629546f0e5317a1d71
Katerine-costa <arielekaterine206@gmail.com>
2025-04-29 18:03:55 -0300

    a

---
+++ 2025-04-24/2025-04-29/exemplo02.py
@@ -0,0 +1,11 @@
+print('calcular área do círculo')
+
+raio= 5
+
+área = 3.14 * raio ** 2
+
+print ('Área do círculo:', área)
+
+print('A área do círculo de raio', raio, 'é', área)
+
+print(f'A área do círculo de raio {raio} é {área:.4f}')
---
+++ 2025-04-24/2025-04-29/exemplo03.py
@@ -0,0 +1,8 @@
+print('Cálculo da média aritmética de 4 notas')
+
+nota1 = 6.5
+nota2 = 7.0
+nota3 = 4.9
+nota4 = 6.3
+
+média = (nota1 + nota2 + nota3 + nota4) / 4
