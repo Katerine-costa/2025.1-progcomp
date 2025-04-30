ad95bb48118e7a2d808ad5b70dfd18466624a990
Katerine-costa <arielekaterine206@gmail.com>
2025-04-29 22:13:15 -0300

    https://github.com/Katerine-costa/2025.1-progcomp

---
+++ 2025-04-29/aula02.py
@@ -0,0 +1,11 @@
+print('Calcular area do circulo')
+
+raio = 5.13
+
+area = 3.14 * raio ** 2
+
+print('Area do circulo: ', area)
+
+print('A area do circulo de raio', raio, 'é', area)
+
+print(f'A area do circulo de raio {raio} é {area:.4f}')
