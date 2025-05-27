2063c45d79b418d6c72e45143c512f133f691873
Katerine-costa <arielekaterine206@gmail.com>
2025-05-27 17:49:14 -0300

    c

---
+++ 2025-05-27/ex2.py
@@ -0,0 +1,14 @@
+'''
+programa para executar um produto de 2 numeros inteiros positivos utilizando apenas o operador soma (+) 
+exemplo: 5 * 7 = 7 + 7 + 7 + 7 + 7
+
+'''
+
+num1 = int(input('digite quantas vezes voce quer que eu some: '))
+num2 = int(input('digite um numero inteiro positivo: '))
+x = 1
+while x <= num1:
+ num2+num2
+ x+=1
+
+print(f' soma dos algarismos é: ', num2)
