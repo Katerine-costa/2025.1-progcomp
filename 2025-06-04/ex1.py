7d2855c64e607b7bf009bd7588aae37f29507e4c
katerine-costa <arielekaterine206@gmail.com>
2025-06-05 10:41:33 -0300

    c

--- 2025-06-04/ex1.py
+++ 2025-06-04/ex1.py
@@ -6,18 +6,25 @@
 import sys
 
 try:
-    num1 = int(input('Digite um numero inteiro positivo: '))
-    num2 = int(input('Digite um numero inteiro positivo: '))
+    num1 = int(input('Digite o primeiro numero inteiro positivo: '))
+    num2 = int(input('Digite o segundo numero inteiro positivo: '))
 except ValueError:
     sys.exit('ERRO: não foi informado um valor inteiro válido...')
 except Exception as e:
     sys.exit(f'ERRO {e}')
 
 else:
-    if num1 > num2 :
-        divisao = num1 // num2
-        resto = (num1 % num2) == 0
-        print(f'MDC é divisor. ')
+    if num1 < 0:
+        sys.exit('ERRO: Digite um número positivo ...')
 
-    else:
-         sys.exit('ERRO: o {num1} e {num2} ')
+    if num2 < 0:
+        sys.exit('ERRO: Digite um número positivo ...')
+
+# 1000 é um valor grande
+
+for _ in range(1000):
+    resto = num1 % num2
+    if resto == 0:
+        print(f'o MDC é {num2}')
+        break  # o break vai interromper o laço assim que encontrar o MDC
+    num1, num2 = num2, resto
