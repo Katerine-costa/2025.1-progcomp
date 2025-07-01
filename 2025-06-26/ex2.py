8dcd5dae217a714b180a0889d3b80f8821902d1e
Katerine-costa <arielekaterine206@gmail.com>
2025-07-01 17:31:53 -0300

    c

--- 2025-06-26/ex2.py
+++ 2025-06-26/ex2.py
@@ -41,12 +41,19 @@ lstTimes = [
 # ----------------------------------------------------------------------
 # Questão 01
 
+for time in lstTimes:
+   time.insert(4, time[1]*3 + time[2])
+   time.append(time[5] - time[6])
 
 # ----------------------------------------------------------------------
 # Questão 02
 # TODO: Fazer na sala de aula no dia 01/07/2026
 # TODO: Pesquisar a função SORT() usando funções LAMBDA
 
+lstTimes.sort(key=lambda time: (time[4], time[1], time[7], time[5]), reverse=True)
 
 # ----------------------------------------------------------------------
 # Questão 03
+
+for time in lstTimes:
+   print(time)
