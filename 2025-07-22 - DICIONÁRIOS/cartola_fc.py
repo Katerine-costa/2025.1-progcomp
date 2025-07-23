5b80b03415daf120d65a2d44876cad4aabbea62e
Katerine-costa <arielekaterine206@gmail.com>
2025-07-23 17:57:39 -0300

    dicionarios

--- 2025-07-22 - DICIONÁRIOS/cartola_fc.py
+++ 2025-07-22 - DICIONÁRIOS/cartola_fc.py
@@ -1,4 +1,3 @@
-from logging import exception
 import os, sys, json
 
 strDirApp = os.path.dirname(__file__)
@@ -6,19 +5,30 @@ strDirApp = os.path.dirname(__file__)
 strNomeArq = f'{strDirApp}//cartola_fc_2024.json'
 
 try:
-    arqinput = open(strNomeArq, 'r', encoding='utf-8')
-    strDados = arqinput.read()
-    dictCartola = json.loads(strDados)
-    arqinput.close()
-except FileNotFoundError:
-    sys.exit('ERRO: Arquivo não existe...')
+   arqInput = open(strNomeArq, 'r', encoding='utf-8')
+   strDados = arqInput.read()
+   dictCartola = json.loads(strDados)
+   arqInput.close()
+except  FileNotFoundError:
+   sys.exit('ERRO: Arquivo não existe...')
 except json.JSONDecodeError:
-    sys.exit('ERRO: o conteudo do arquivo não esta no formato correto.')
-except exception as erro:
-    sys.exit(f'ERRO {erro}...')
+   sys.exit('ERRO: O conteúdo do arquivo não está no formato correto...')
+except Exception as erro:
+   sys.exit(f'ERRO: {erro}...')
 else:
-    lstChaves = list(dictCartola.keys())
-    print(lstChaves)
-    print(dictCartola{'atletas'})
-finally:
-    print('\nFIM DO PROGRAMA!')
+   lstChaves = list(dictCartola.keys()) # ['clubes', 'posicoes', 'status', 'atletas']
+
+   '''
+      dictCartola = { 'clubes': {...}, 'posicoes': {...}, 'status':{...}, 'atletas': [...] }
+
+      dictCartola['clubes']    -> dicionário (k:v)
+      dictCartola['posicoes']  -> dicionário (k:v)
+      dictCartola['status']    -> dicionário (k:v)
+      dictCartola['atletas']   -> lista (índice -> dicionário (k:v))
+   '''
+   # Informando o nome do Clube
+   strClube = input('\nInforme o nome do Clube: ').strip().lower()
+
+   # Obtendo o ID do clube informado
+
+   # Listando os atletas do Clube informado
